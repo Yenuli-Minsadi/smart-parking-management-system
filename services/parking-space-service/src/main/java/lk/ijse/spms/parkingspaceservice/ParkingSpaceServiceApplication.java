ac1dc888b0e3b3fa4e519223af4338c104145f59
Yenuli Minsadi <samararatney@gmail.com>
2026-08-12 14:14:58 +0530

    feat: enable eureka client, config server and eureka server annotations in main application classes

--- services/parking-space-service/src/main/java/lk/ijse/spms/parkingspaceservice/ParkingSpaceServiceApplication.java
+++ services/parking-space-service/src/main/java/lk/ijse/spms/parkingspaceservice/ParkingSpaceServiceApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.spms.parkingspaceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ParkingSpaceServiceApplication {
 
     public static void main(String[] args) {
--- services/user-service/src/main/java/lk/ijse/spms/userservice/UserServiceApplication.java
+++ services/user-service/src/main/java/lk/ijse/spms/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package lk.ijse.spms.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
