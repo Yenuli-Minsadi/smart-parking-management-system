bc6c326dc2fcdc14a20558774726993d09bbf313
Yenuli Minsadi <samararatney@gmail.com>
2026-08-26 17:48:13 +0530

    chore(vehicle-service): add vehicle entity

--- services/vehicle-service/src/main/java/lk/ijse/spms/vehicleservice/model/Vehicle.java
+++ services/vehicle-service/src/main/java/lk/ijse/spms/vehicleservice/model/Vehicle.java
@@ -1,4 +1,37 @@
 package lk.ijse.spms.vehicleservice.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "vehicles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String licensePlateNumber;
+
+    @Column(nullable = false)
+    private String model;
+
+    @Column(nullable = false)
+    private String vehicleType;
+
+    @Column(nullable = false)
+    private String ownerId;
+
+    private LocalDateTime entryTime;
+    private LocalDateTime exitTime;
+
+    @Column(nullable = false)
+    private boolean currentlyParked;
 }
