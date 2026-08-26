72b986664251ba02f74a5965f41a2b9b3b432e40
Yenuli Minsadi <samararatney@gmail.com>
2026-08-26 19:48:56 +0530

    chore(vehicle-service): add VehicleRequestDTO and VehicleResponseDTO

--- services/vehicle-service/src/main/java/lk/ijse/spms/vehicleservice/dto/VehicleRequestDTO.java
+++ services/vehicle-service/src/main/java/lk/ijse/spms/vehicleservice/dto/VehicleRequestDTO.java
@@ -1,4 +1,27 @@
 package lk.ijse.spms.vehicleservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class VehicleRequestDTO {
+
+    @NotBlank(message = "License plate number is required")
+    private String licensePlateNumber;
+
+    @NotBlank(message = "Model is required")
+    private String model;
+
+    @NotBlank(message = "Vehicle type is required")
+    private String vehicleType;
+
+    @NotBlank(message = "Owner Id is required")
+    private Long ownerId;
+
 }
--- services/vehicle-service/src/main/java/lk/ijse/spms/vehicleservice/dto/VehicleResponseDTO.java
+++ services/vehicle-service/src/main/java/lk/ijse/spms/vehicleservice/dto/VehicleResponseDTO.java
@@ -1,4 +1,25 @@
 package lk.ijse.spms.vehicleservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class VehicleResponseDTO {
+
+    private Long id;
+    private String licensePlateNumber;
+    private String model;
+    private String vehicleType;
+    private Long ownerId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean currentlyParked;
+
 }
