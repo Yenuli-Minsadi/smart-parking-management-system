9ea8366fce2e42a308a89a5ba17da4c3552c8c19
Yenuli Minsadi <samararatney@gmail.com>
2026-08-27 09:35:15 +0530

    feat(vehicle-service): register method implementation in VehicleController

--- services/vehicle-service/src/main/java/lk/ijse/spms/vehicleservice/controller/VehicleController.java
+++ services/vehicle-service/src/main/java/lk/ijse/spms/vehicleservice/controller/VehicleController.java
@@ -1,4 +1,27 @@
 package lk.ijse.spms.vehicleservice.controller;
 
+import jakarta.validation.Valid;
+import lk.ijse.spms.vehicleservice.dto.VehicleRequestDTO;
+import lk.ijse.spms.vehicleservice.dto.VehicleResponseDTO;
+import lk.ijse.spms.vehicleservice.service.custom.VehicleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/vehicles")
+@RequiredArgsConstructor
 public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    @PostMapping
+    public ResponseEntity<VehicleResponseDTO> register(@Valid @RequestBody VehicleRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(vehicleService.register(dto));
+    }
+
 }
