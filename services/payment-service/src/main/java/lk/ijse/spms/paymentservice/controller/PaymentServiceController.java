2aecda696932995df4019a07ecda69a79e513ebe
Yenuli Minsadi <samararatney@gmail.com>
2026-08-29 22:30:24 +0530

    feat(payment-service): scaffold package structure

---
+++ services/payment-service/src/main/java/lk/ijse/spms/paymentservice/controller/PaymentServiceController.java
@@ -0,0 +1,4 @@
+package lk.ijse.spms.paymentservice.controller;
+
+public class PaymentServiceController {
+}
---
+++ services/payment-service/src/main/java/lk/ijse/spms/paymentservice/model/PaymentService.java
@@ -0,0 +1,4 @@
+package lk.ijse.spms.paymentservice.model;
+
+public class PaymentService {
+}
